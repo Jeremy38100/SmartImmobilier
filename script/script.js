dbc2df7b5ae6ab5731ca8425df5ec7d4669c6103
jeremy <jeremy.roche@situ8ed.com>
2019-08-26 11:02:00 +0200

    projection on angles

--- script/script.js
+++ script/script.js
@@ -1,18 +1,70 @@
 window.onload = function() {
-  start(); 
+  start();
 }
 
-async function start() {
-  const data = await load('data.json');
-  const chart = c3.generate({
+async function displayChart(points) {
+  c3.generate({
     bindto: '#chart',
     data: {
-      columns: [
-        data
-      ]
+      json: points,
+      keys: {
+          value: ['altitude', 'angle'],
+      },
+      axes: {
+        altitude: 'y',
+        angle: 'y2',
+      },
+      onmouseover: overPointChart,
+    },
+    axis: {
+      y2: { show: true }
+    }
+  });
+  c3.generate({
+    bindto: '#chart2',
+    data: {
+      json: points,
+      keys: {
+          value: ['distance', 'angle'],
+      },
+      axes: {
+        distance: 'y',
+        angle: 'y2',
+      },
+      onmouseover: overPointChart,
+    },
+    axis: {
+      y2: { show: true }
     }
   });
 }
+
+let map;
+async function displayMap(points) {
+  map = L.map('map').setView([45.217784, 5.807527], 10);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  const latlngs = points.map(p => [p.latitude, p.longitude]);
+  const polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+}
+
+let points = [];
+async function start() {
+  points = await load('data.json');
+  displayChart(points);
+  displayMap(points);
+}
+
+let lastMarker = null;
+function overPointChart(pointData) {
+  if (lastMarker) map.removeLayer(lastMarker);
+  const point = points[pointData.index];
+  lastMarker = L.marker([point.latitude, point.longitude]).addTo(map);
+}
+
 
 function load(url) {
   return new Promise(resolve => {
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const {performance} = require('perf_hooks');
 const geolib = require('geolib');
 const elevation = require('./elevation');
 const fs = require('fs');
@@ -6,32 +7,66 @@ const origin = {
   latitude: 45.217784,
   longitude: 5.807527
 };
-const projections = [];
 
 const DISTANCE_TOTAL_M = 50000;
-const OFFSET_M = 20
+const OFFSET_M = 50
+const MIN_BEARING = 90;
 const MAX_BEARING = 270;
-const BEARING_OFFSET = 1;
-let bearing = 90;
+const BEARING_OFFSET = 0.5;
 
 async function start() {
-  while (bearing <= MAX_BEARING ) {
+
+  origin.altitude = await elevation(origin.latitude, origin.longitude);
+  const t0 = performance.now();
+  const bearings = getBearings();
+  remaining = bearings.length;
+  Promise.all(bearings.map(highestPointInBearing))
+    .then(projections => {
+      const t1 = performance.now();
+      console.log((t1 - t0) + " ms.")
+      fs.writeFileSync('data.json', JSON.stringify(projections));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+function getBearings() {
+  const bearings = [];
+  let bearing = MIN_BEARING;
+  while (bearing <= MAX_BEARING) {
+    bearings.push(bearing)
+    bearing += BEARING_OFFSET;
+  }
+  return bearings;
+}
+
+function highestPointInBearing(bearing) {
+  return new Promise(async resolve => {
     let highestPoint = null;
-    for (let i = 0; i < (DISTANCE_TOTAL_M / OFFSET_M); i++) {
-      const newPoint = geolib.computeDestinationPoint(origin, OFFSET_M * (i+1), bearing);
-      const altitude = await elevation(newPoint.latitude, newPoint.longitude);
-      if (!highestPoint || altitude > highestPoint.altitude) {
+    for (let i = 1; i < (DISTANCE_TOTAL_M / OFFSET_M); i++) {
+      const distance = OFFSET_M * i;
+      const newPoint = geolib.computeDestinationPoint(origin, distance, bearing);
+
+      let altitude = Math.round(await elevation(newPoint.latitude, newPoint.longitude));
+      let angle = getAngle(altitude - origin.altitude, distance) * 180/Math.PI;
+      if (angle < 0) angle = 0;
+
+      if (!highestPoint || angle > highestPoint.angle) {
+      // if (!highestPoint || altitude > highestPoint.altitude) {
         highestPoint = {
           latitude: newPoint.latitude,
           longitude: newPoint.longitude,
-          altitude: altitude,
+          altitude,
+          angle,
+          distance
         }
       }
     }
-    projections.push(highestPoint);
-    bearing += BEARING_OFFSET;
-  }
-  fs.writeFileSync('data.json', JSON.stringify(projections.map(e => e.altitude)));
+    resolve(highestPoint);
+  });
 }
+
+function getAngle(height, distance) { return Math.atan(height / distance); }
 
 start();
