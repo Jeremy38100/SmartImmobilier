9709db7dfade1229e74e77511346a682ce479077
Jeremy38100 <jeremyroche5@gmail.com>
2019-08-28 18:59:34 +0200

    init map

--- SmartImmobilier/src/app/tab1/tab1.module.ts
+++ SmartImmobilier/src/app/tab1/tab1.module.ts
@@ -1,3 +1,4 @@
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
@@ -10,6 +11,7 @@ import { Tab1Page } from './tab1.page';
     IonicModule,
     CommonModule,
     FormsModule,
+    LeafletModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
--- SmartImmobilier/src/app/tab1/tab1.page.ts
+++ SmartImmobilier/src/app/tab1/tab1.page.ts
@@ -1,15 +1,36 @@
 import { SunPositionService } from './../service/sun-position.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { tileLayer, latLng, Map } from 'leaflet';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  map: Map;
+
+  ngOnInit(): void {
+    console.log('init');
+  }
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 10,
+    center: latLng(45.178728, 5.749591)
+  };
 
   constructor(private sun: SunPositionService) {
     this.getSunPositions();
+  }
+
+  onMapReady(map: Map) {
+    window.dispatchEvent(new Event('resize'));
+    this.map = map;
+    map.on('click', (e) => { console.log(e); });
   }
 
   async getSunPositions() {
@@ -18,5 +39,4 @@ export class Tab1Page {
       {year: 2019, month: 11, day: 21}
     ));
   }
-
 }
